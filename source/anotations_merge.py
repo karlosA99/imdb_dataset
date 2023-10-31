f912c52d78cd7933690181ac8846fcd92a82d950
Karlos Alejandro Alfonso <karlos.bruno99@gmail.com>
2023-10-31 12:40:03 -0400

    merge anotations

--- source/anotations_merge.py
+++ source/anotations_merge.py
@@ -3,10 +3,17 @@ import pandas as pd
 empty_revs_path = 'data/extended_reviews.csv'
 
 def equal_decisions(datasets, idx, col):
-    for i in range(len(datasets)-1):
-        if datasets[i].loc[idx, col] != datasets[i+1].loc[idx, col]:
-            return False
-    return True
+    are_equals = True
+    decisions = []
+    size = len(datasets)
+    
+    for i in range(size):
+        decisions.append(datasets[i].loc[idx, col])
+        
+        if datasets[i%size].loc[idx, col] != datasets[(i+1)%size].loc[idx, col]:
+            are_equals = False
+            
+    return are_equals, decisions
 
 def sort_race(path):
     dataframe = pd.read_csv(path)
@@ -19,6 +26,25 @@ def sort_race(path):
         dataframe.at[idx, 'Race'] = item
     
     dataframe.to_csv(path, index=False, mode='w')
+
+def get_final_decision(decisions, review):
+    print("Ingrese el numero de la opcion que considere mas acertada:")
+    print("Review:")
+    print(review)
+    print("=====================================")
+    print("Decisions:")
+    
+    for i in range(len(decisions)):
+        print(str(i+1) + ": " + decisions[i])
+        
+    print("=====================================")
+    choice = input('Choise:')
+    
+    try:
+        choice = int(choice)-1
+        return decisions[choice]
+    except:
+        return None
     
 def merge(*paths):
     final_reviews = pd.read_csv(empty_revs_path)
@@ -27,15 +53,17 @@ def merge(*paths):
     
     for idx, row in final_reviews.iterrows():
         if pd.isna(final_reviews.loc[idx, 'Gender']):
-            if not equal_decisions(datasets, idx, 'Gender'):
-                #aqui hay que mostrar el review y las anotaciones y que el experto escoja la mas acertada
-                pass
+            are_equals, decisions = equal_decisions(datasets, idx, 'Gender')
+            if not are_equals:
+                final_decision = get_final_decision(decisions, final_reviews.loc[idx, 'Review'])
+                final_reviews.at[idx, 'Gender'] = final_decision
             else:
                 final_reviews.at[idx, 'Gender'] = datasets[0].loc[idx, 'Gender']
             
         if pd.isna(final_reviews.loc[idx, 'Race']):
-            if not equal_decisions(datasets, idx, 'Race'):
-                #aqui hay que mostrar el review y las anotaciones y que el experto escoja la mas acertada
-                pass
+            are_equals, decisions = equal_decisions(datasets, idx, 'Race')
+            if not are_equals:
+                final_decision = get_final_decision(decisions, final_reviews.loc[idx, 'Review'])
+                final_reviews.at[idx, 'Race'] = final_decision
             else:
                 final_reviews.at[idx, 'Race'] = datasets[0].loc[idx, 'Race']
