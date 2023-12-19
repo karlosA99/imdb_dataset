5e6df3ae498896c0570051f46f0bde4464477b63
Karlos Alejandro Alfonso <karlos.bruno99@gmail.com>
2023-12-18 21:35:43 -0500

    print stats

--- source/ds_statistics.py
+++ source/ds_statistics.py
@@ -31,38 +31,69 @@ count_gender = count_in_gender(ds)
 
 #Total de reviews
 total = ds.shape[0]
+print("Total de reviews:", total)
 
-#Cantidad de reviews con genero null
+# Cantidad de reviews con genero null
 cnt_gender_null = count_gender['Null']
-#Cantidad de reviews con genero no null
-cnt_gender_not_null = total - cnt_gender_null
-#Cantidad de reviews con genero exclusivamente masculino
-cnt_gender_only_male = count_gender["Male"]
-#Cantidad de reviews con genero exclusivamente femenino
-cnt_gender_only_female = count_gender["Female"]
-#Cantidad de reviews con genero masculino y femenino
-cnt_gender_mix = count_gender["Male, Female"]
-#Cantidad de reviews con genero masculino
-cnt_gender_male = cnt_gender_only_male + cnt_gender_mix
-#Cantidad de reviews con genero femenino
-cnt_gender_female = cnt_gender_only_female + cnt_gender_mix
+print("Cantidad de reviews con genero null:", cnt_gender_null)
 
-#Cantidad de reviews con raza null
+# Cantidad de reviews con genero no null
+cnt_gender_not_null = total - cnt_gender_null
+print("Cantidad de reviews con genero no null:", cnt_gender_not_null)
+
+# Cantidad de reviews con genero exclusivamente masculino
+cnt_gender_only_male = count_gender["Male"]
+print("Cantidad de reviews con genero exclusivamente masculino:", cnt_gender_only_male)
+
+# Cantidad de reviews con genero exclusivamente femenino
+cnt_gender_only_female = count_gender["Female"]
+print("Cantidad de reviews con genero exclusivamente femenino:", cnt_gender_only_female)
+
+# Cantidad de reviews con genero masculino y femenino
+cnt_gender_mix = count_gender["Male, Female"]
+print("Cantidad de reviews con genero masculino y femenino:", cnt_gender_mix)
+
+# Cantidad de reviews con genero masculino
+cnt_gender_male = cnt_gender_only_male + cnt_gender_mix
+print("Cantidad de reviews con genero masculino:", cnt_gender_male)
+
+# Cantidad de reviews con genero femenino
+cnt_gender_female = cnt_gender_only_female + cnt_gender_mix
+print("Cantidad de reviews con genero femenino:", cnt_gender_female)
+
+# Cantidad de reviews con raza null
 cnt_race_null = count_race['Null']
-#Cantidad de reviews con raza no null
+print("Cantidad de reviews con raza null:", cnt_race_null)
+
+# Cantidad de reviews con raza no null
 cnt_race_not_null = total - cnt_race_null
-#Cantidad de reviews con raza white
+print("Cantidad de reviews con raza no null:", cnt_race_not_null)
+
+# Cantidad de reviews con raza white
 cnt_race_white = count_race['White']
-#Cantidad de reviews con raza black
+print("Cantidad de reviews con raza white:", cnt_race_white)
+
+# Cantidad de reviews con raza black
 cnt_race_black = count_race['Black']
-#Cantidad de reviews con raza asian
+print("Cantidad de reviews con raza black:", cnt_race_black)
+
+# Cantidad de reviews con raza asian
 cnt_race_asian = count_race['Asian']
-#Cantidad de reviews con raza latino
+print("Cantidad de reviews con raza asian:", cnt_race_asian)
+
+# Cantidad de reviews con raza latino
 cnt_race_latino = count_race['Latino']
-#Cantidad de reviews con raza indian
+print("Cantidad de reviews con raza latino:", cnt_race_latino)
+
+# Cantidad de reviews con raza indian
 cnt_race_indian = count_race['Indian']
-#Cantidad de reviews con raza native
+print("Cantidad de reviews con raza indian:", cnt_race_indian)
+
+# Cantidad de reviews con raza native
 cnt_race_native = count_race['Native American']
-#Cantidad de reviews con raza arab
+print("Cantidad de reviews con raza native:", cnt_race_native)
+
+# Cantidad de reviews con raza arab
 cnt_race_arab = count_race['Arab']
+print("Cantidad de reviews con raza arab:", cnt_race_arab)
 
